03c481416d1a4049e952ebc112a013b7d46a1db9
baotr10 <tranbeo10032004@gmail.com>
2026-06-13 13:46:59 +0700

    Bao feat/register: finish view blog and view blog detail

--- src/routes/customer/index.js
+++ src/routes/customer/index.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const authRoutes = require('./auth.routes');
+const blogRoutes = require('./blog.routes');
 
 const router = express.Router();
 
 router.use('/auth', authRoutes);
+router.use('/blogs', blogRoutes)
 
 module.exports = router;
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,5 @@
 const express = require('express');
-
 const customerRoutes = require('./customer');
-
 const router = express.Router();
 
 router.use('/customer', customerRoutes);
